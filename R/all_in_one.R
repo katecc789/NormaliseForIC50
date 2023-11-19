32b09274926302eac612b057c6369fe21b420923
Mark Cheng <89597443+TKMarkCheng@users.noreply.github.com>
2023-11-19 13:22:02 +0000

    Update all_in_one.R - bugfix
    control_pos_column and control_neg_column are now user definable

--- R/all_in_one.R
+++ R/all_in_one.R
@@ -1,21 +1,21 @@
 #' Strings all functions together.
 #'
 #' @param df dataframe to normalise, where left most column is negative control, and second left column is positive control.
-#' @param control_neg The column which negative/cell control are located (column number from left, integer,default=1)
-#' @param control_pos The column which positive/cell control are located (column number from left, integer,default=2)
+#' @param control_neg_column The column which negative/cell control are located (column number from left, integer,default=1)
+#' @param control_pos_column The column which positive/cell control are located (column number from left, integer,default=2)
 #' @param rotation_deg_needed degrees that dataframe needs to be rotated, default =0. Feeds into rotate.R
 #' @return A rotated, normalised dataframe where negative and positive control columns are removed.
 #' @export
 
 #final function
-final_func <- function(neut_xlsx_path, sheetname="Results", rotation_deg_needed=0){
+final_func <- function(neut_xlsx_path, control_neg_column=1, control_pos_column=2,sheetname="Results", rotation_deg_needed=0){
   print(paste0("processing input file: ",neut_xlsx_path))
   # 1. read in and select relevant 8x12 area of the excel Results sheet
   df <- read_promega_plate_excel(neut_xlsx_path)
   # 2. rotate by n degrees, if needed
   df <- rotate(df,rotation_deg_needed = rotation_deg_needed)
   # 3. normalize the rotated df
-  output <- normalise(df = df,control_neg_column = 1,control_pos_column = 2) #change if negative and positive column not on far left
+  output <- normalise(df = df,control_neg_column = control_neg_column,control_pos_column = control_pos_column) #change if negative and positive column not on far left
   # 4. Return and announce completion
   return(output)
   input_name = tools::file_path_sans_ext(neut_xlsx_path)
