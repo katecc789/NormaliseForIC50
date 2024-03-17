af64b0b5f6dc43cec550060161b236e1e556a5d8
TKMCheng <89597443+TKMarkCheng@users.noreply.github.com>
2024-03-17 02:52:07 +0000

    Updated generate_plate_map so Well A3-A12 are generated automatically in order for each plate.

--- R/plate_map.R
+++ R/plate_map.R
@@ -2,20 +2,28 @@
 #' @title Read in and validates, or generate PlateMap following strict formatting as Example_PlateMap.xlsx
 #' @aliases validate_plate_map
 #' @aliases generate_plate_map
-#' @param plateMap_file PlateMap excel file name
+#' @param plateMap_file PlateMap excel file name to read in.
 #' @param sheet the sheet which the wanted PlateMap resides on in the plateMap_file
+#' @param read_mode "default" which silently adds missing column or "debug" which is invoked which provides warning of missing/unexpected columns.  
 #' @param directory_of_files the directory_of_files that is iterated through to look for Promega output files to generated output_plateMap_file.
 #' @param output_plateMap_file filepath where the generated PlateMap will be located.
+#' @param output_mode "return" which returns the generated PlateMap into a dataframe object, or "write" which writes to an excel file. 
 #' @importFrom magrittr %>%
 
 expected_columns <- c("Group","Negative_Control_Column","Positive_Control_Column",
              "Individual_condition","Virus","Plate","Plate_Name","Well","dilution_or_concentration","Starting_Dilution_or_concentration","dilution_series")
+wells <- paste0("A", rep(3:12)) # assuming vertical serial dilutions
 
+#' @rdname plate_map_functions
 #' @section general:
-validate_plate_map <- function(df,plateMap_file,mode="default"){
+#' The validate function is required in both reading in and writing out to make sure the expected_columns are included.
+#' @description
+#' Validates the PlateMap by checking if there are unexpected or missing columns, and if there are missing columns, adding them.
+#' 
+validate_plate_map <- function(df,plateMap_file,read_mode="default"){
   # keep and check unexpected columns
   unexpected_columns <- setdiff(colnames(df),expected_columns)
-  if (mode=="debug"){
+  if (read_mode=="debug"){
     warning(glue::glue("detected {length(unexpected_columns)} unexpected_columns:{paste(unexpected_columns,collapse=',')} in {plateMap_file}, please double check if you made any changes to expected column headers."))
     }
   # If there are missing columns, add them to the df
@@ -23,7 +31,7 @@ validate_plate_map <- function(df,plateMap_file,mode="default"){
   if (length(missing_columns) > 0) {
     for (col in missing_columns) {
       df[[col]] <- NA  # You can replace NA with any default value if needed
-      if (mode=="debug"){warning(glue::glue("Your platemap {plateMap_file} is missing '{col}'. Adding a '{col}' column with empty values."))}
+      if (read_mode=="debug"){warning(glue::glue("Your platemap {plateMap_file} is missing '{col}'. Adding a '{col}' column with empty values."))}
     }
   }
   # Reorder df with the ORDERED expected columns and unexpected columns to the list
@@ -31,6 +39,7 @@ validate_plate_map <- function(df,plateMap_file,mode="default"){
   return(df)
 }
 
+#' @rdname plate_map_functions
 #' @section read-created-plate-map:
 #' @export 
 read_validate_plate_map <- function(plateMap_file,sheet="Sheet1"){
@@ -42,6 +51,7 @@ read_validate_plate_map <- function(plateMap_file,sheet="Sheet1"){
   return(df)
 }
 
+#' @rdname plate_map_functions
 #' @section generate-plate-map: 
 #' 
 find_promega_plate_paths <- function(directory_of_files){
@@ -57,6 +67,7 @@ find_promega_plate_paths <- function(directory_of_files){
   return(promega_excel_files)
 }
 
+#' @rdname plate_map_functions
 #' @section generate-plate-map:
 #' @examples
 #' get_read_date("Validation/2022-09-04 reads/example1.xlsx")
@@ -66,28 +77,42 @@ get_read_date <- function(file){
   return(execution_date)
 }
 
+#' @rdname plate_map_functions
 #' @section generate-plate-map:
-#' @rdname generate_plate_map
-#' @param mode "return" will return into a variable, "write" will write to excel file defined by output_plateMap_file
-#' @returns A dataframe, or an excel file with two additional columns. 1) The file path of the plates, and 2) the file creation date which corresponds to read date.
+#' @param output_mode "return" will return into a dataframe variable, "write" will write to excel file defined by output_plateMap_file
+#' @returns A dataframe, or an excel file with Wells prepared and two additional columns. 1) The file path of the plates, and 2) the file creation date which corresponds to read date.
 #' @examples
 #' generate_plate_map("Validation/")
 #' @export
-generate_plate_map <- function(directory_of_files,output_plateMap_file="Validation/generated_platemap.xlsx",mode="return"){
+generate_plate_map <- function(directory_of_files,output_plateMap_file="Validation/generated_platemap.xlsx",output_mode="return"){
   promega_plate_paths <- find_promega_plate_paths(directory_of_files)
   promega_plate_path <- data.frame(promega_plate_paths) %>% dplyr::rename(promega_plate_path=promega_plate_paths)
   
   output_plateMap <- promega_plate_path %>% 
     dplyr::mutate(Plate_Name=
-                    tools::file_path_sans_ext(basename(promega_plate_path))
+                    tools::file_path_sans_ext(basename(promega_plate_path)) # strip plate_path to get Plate_Name
                     )%>%
-    dplyr::rowwise()%>% dplyr::mutate(file_creation_date=get_read_date(promega_plate_path))
-    
+    dplyr::rowwise()%>% dplyr::mutate(file_creation_date=
+                                        get_read_date(promega_plate_path)) #add file creation date
+  # validate to match format
   output_plateMap <- validate_plate_map(output_plateMap)
-  if (mode=="return"){
+  
+  # expand rows to generate wells that people can fill manually
+  plate_expanded_wells <- tidyr::crossing(
+    Plate_Name = unique(output_plateMap$Plate_Name),
+    Well=wells
+  )
+  output_plateMap <- merge(output_plateMap %>% dplyr::select(-Well),
+                           plate_expanded_wells
+  ) %>%
+    dplyr::arrange(as.numeric(gsub("\\D","",Plate_Name)),
+                   as.numeric(gsub("\\D","",Well))
+    )
+  
+  if (output_mode=="return"){
     return (output_plateMap)
-  } else if (mode=="write"){
-    openxlsx::write.xlsx(x=output_plateMap,file = output_plateMap_file)
+  } else if (output_mode=="write"){
+    openxlsx::write.xlsx(x=output_plateMap,file = output_plateMap_file,overwrite = FALSE)
     message(glue::glue("Generated platemap based on Promega files in {directory_of_files} written to {output_plateMap_file}"))
   }
 }
